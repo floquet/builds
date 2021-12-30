3f775d06516c0474712b2c0c299819ec466bfb0f
Daniel M. Topa <dantopa@gmail.com>
2021-12-29 20:50:45 -0700

    Let's try global variables

--- scripts-docker/kickstarts/installers/dnf-installs.sh
+++ scripts-docker/kickstarts/installers/dnf-installs.sh
@@ -9,32 +9,32 @@ printf '%s\n' "$(date) ${BASH_SOURCE[0]}"
 
 new_step "Update, upgrade, install Development Tools"
 sub_step_counter=0
-sub_step "dnf update -v -y | tee -a ${2}/update.txt 2>&1"
-          dnf update -v -y | tee -a ${2}/update.txt 2>&1
+sub_step "dnf update -v -y | tee -a ${dirBuildResults}/update.txt 2>&1"
+          dnf update -v -y | tee -a ${dirBuildResults}/update.txt 2>&1
 
-sub_step "dnf upgrade -v -y | tee -a ${2}/update.txt 2>&1"
-          dnf upgrade -v -y | tee -a ${2}/update.txt 2>&1
+sub_step "dnf upgrade -v -y | tee -a ${dirBuildResults}/update.txt 2>&1"
+          dnf upgrade -v -y | tee -a ${dirBuildResults}/update.txt 2>&1
 
 # https://linuxize.com/post/how-to-install-gcc-on-centos-8/
-sub_step 'dnf group install -v "Development Tools" -y | tee ${2}/update 2>&1'
-          dnf group install -v "Development Tools" -y | tee ${2}/update 2>&1
+sub_step 'dnf group install -v "Development Tools" -y | tee ${dirBuildResults}/update 2>&1'
+          dnf group install -v "Development Tools" -y | tee ${dirBuildResults}/update 2>&1
 
-new_step "Try to build ${#1[@]} packages"
+new_step "Try to build ${#lpackages[@]} packages"
 sub_step_counter=0
-for t in ${1[@]}; do
-    sub_step "dnf install -v ${t} -y | tee -a ${2}/install-${t}.txt 2>&1"
-              dnf install -v ${t} -y | tee -a ${2}/install-${t}.txt 2>&1
+for t in ${lpackages[@]}; do
+    sub_step "dnf install -v ${t} -y | tee -a ${dirBuildResults}/install-${t}.txt 2>&1"
+              dnf install -v ${t} -y | tee -a ${dirBuildResults}/install-${t}.txt 2>&1
 
-              dnf info ${t}  > ${2}/info-${t}.txt &
+              dnf info ${t}  > ${dirBuildResults}/info-${t}.txt &
 done
 
 new_step "Prepare summary reports"
 sub_step_counter=0
-sub_step "dnf list available > ${2}/list-available.txt"
-          dnf list available > ${2}/list-available.txt &
+sub_step "dnf list available > ${dirBuildResults}/list-available.txt"
+          dnf list available > ${dirBuildResults}/list-available.txt &
 
-sub_step "dnf list installed > ${2}/list-installed.txt"
-          dnf list installed > ${2}/list-installed.txt &
+sub_step "dnf list installed > ${dirBuildResults}/list-installed.txt"
+          dnf list installed > ${dirBuildResults}/list-installed.txt &
 
-sub_step "dnf list repo      > ${2}/list-repo.txt"
-          dnf list repo      > ${2}/list-repo.txt      &
+sub_step "dnf list repo      > ${dirBuildResults}/list-repo.txt"
+          dnf list repo      > ${dirBuildResults}/list-repo.txt      &
