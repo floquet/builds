2b36fb3b227bfc91164b4453ef01f20dc42a1bed
Daniel M. Topa <dantopa@gmail.com>
2021-12-31 17:39:49 -0700

    xiuhcoatl native Monterey-12.1

--- scripts-docker/spack-build-out/master-header.sh
+++ scripts-docker/spack-build-out/master-header.sh
@@ -19,25 +19,28 @@ function sub_step(){
 }
 
 # spacktion ${p} ${cmd_arguments} ${log_file} ${bspec}
+# https://stackoverflow.com/questions/1983048/passing-a-string-with-spaces-as-a-function-argument-in-bash
 function spacktion(){
     # string arguments with spacing require eval
     eval    package=$1
     eval spack_args=$2
     eval   log_file=$3
     eval  spec_file=$4
-if ! command -v ${1} &> /dev/null
 # 1. Build with package manager (yum, apt-get etc.), otherwise...
 # 2. Build with Spack
+if ! command -v ${1} &> /dev/null
 then
+    # build with spack
     echo ""
     echo "No system definition for ${package} in ${dist}-${release}."
     echo "Using Spack to build ${package}."
 
     sub_step "spack install ${package} ${spack_args}  > ${log_file}"
     echo     "spack install ${package} ${spack_args}" > ${log_file}
-              #spack install ${package} ${spack_args}  | tee -a ${log_file} 2>&1
-              #spack spec    ${package} ${spack_args}  >        "${spec_file}/${package}.txt" &
+              spack install ${package} ${spack_args}  | tee -a ${log_file} 2>&1
+              spack spec    ${package} ${spack_args}  >       "${spec_file}/${package}.txt" &
 else
+    # find existing build
     echo "Application ${package} already installed."
     echo "command -v ${package}"
           command -v ${package}
