f20072b4dc955c54d14b9effe84ce4931d827988
Daniel Topa <dantopa@gmail.com>
2022-02-17 14:49:56 -0700

    Scan SpWx centos7

---
+++ results-spack/linux-centos7-haswell/MacBookPro16,1-(ehecoatl)/linux/centos-7.9.2009/reaper/2022-02-17_14,48/dantopa/shell-scripts/common-header.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
+
+# Wed Dec 29 18:20:31 MST 2021
+
+# timestamp results
+export ymdt=$(date +%Y-%m-%d\ %H:%M) # timestamp results
+
+# enumerate steps
+function new_step(){
+    step_counter=$((step_counter+1))
+    echo ""
+    echo "Step ${step_counter}: ${1}"
+}
+
+# enumerate subsubsteps
+function sub_step(){
+    sub_step_counter=$((sub_step_counter+1))
+    echo ""
+    echo "  ${step_counter}.${sub_step_counter}: ${1}"
+}
+
+# enumerate substeps
+function sub_sub_step(){
+    sub_sub_step_counter=$((sub_sub_step_counter+1))
+    echo ""
+    echo "  ${step_counter}.${sub_step_counter}.${sub_sub_step_counter}: ${1}"
+}
