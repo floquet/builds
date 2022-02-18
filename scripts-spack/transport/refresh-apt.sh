cb18e54a79c53a9c788d472037673102f1bd5869
Daniel Topa <dantopa@gmail.com>
2022-02-17 19:59:15 -0700

    Dockering away

--- scripts-spack/transport/refresh-apt.sh
+++ scripts-spack/transport/refresh-apt.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
-
 # generated on 2022-01-10 16:58:49
 
 export elapsed=${SECONDS}
@@ -8,20 +7,20 @@ export elapsed=${SECONDS}
 echo "begin: $(date)"
 echo ""
 
-echo "Step 1 of 2: yum update"
+echo "Step 1 of 2: apt-get update"
 echo ""
 
 date > results-update.txt
-echo "yum update 2>&1 | tee -a results-update.txt" >> results-update.txt
-      yum update 2>&1 | tee -a results-update.txt
+echo "apt-get update 2>&1 | tee -a results-update.txt" >> results-update.txt
+      apt-get update 2>&1 | tee -a results-update.txt
 echo ""
 
-echo "Step 2 of 2: yum update"
+echo "Step 2 of 2: apt-get upgrade"
 echo ""
 
 date > results-upgrade.txt
-echo "yum upgrade 2>&1 | tee -a results-upgrade.txt" >> results-upgrade.txt
-      yum upgrade 2>&1 | tee -a results-upgrade.txt
+echo "apt-get upgrade -y 2>&1 | tee -a results-upgrade.txt" >> results-upgrade.txt
+      apt-get upgrade -y 2>&1 | tee -a results-upgrade.txt
 echo ""
 
 echo "results posted to results-update.txt and results-upgrade.txt"
@@ -33,4 +32,4 @@ echo ""
 
 export elapsed=$((${SECONDS}-${elapsed}))
 
-printf "time to refresh yum distribution: %dh:%dm:%ds\n" $(($elapsed/3600)) $(($elapsed%3600/60)) $(($elapsed%60))
+printf "time to refresh apt distribution: %dh:%dm:%ds\n" $(($elapsed/3600)) $(($elapsed%3600/60)) $(($elapsed%60))
