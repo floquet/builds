0bf1d2546d29908cec697a2dc017b0eac241da3f
Daniel M. Topa <dantopa@gmail.com>
2022-03-18 22:37:28 -0600

    set-env

--- scripts-spack/environment/set-environment.sh
+++ scripts-spack/environment/set-environment.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
-export myCompiler=" % gcc@11.2.0_3"
+# export myCompiler=" % gcc@11.2.0_3"
 # export myCompiler=" % gcc@12.0.1"
-# export myCompiler=" % gcc@7.3.1"
-export myLLVM=" ^llvm@13.0.1"
-export myOpenMPI=" ^openmpi@4.1.2"
-export myPython=" ^python@3.10.2"
+export  myCompiler=" % gcc@7.3.1"
+export      myLLVM=" ^llvm@13.0.1"
+export   myOpenMPI=" ^openmpi@4.1.2"
+export    myPython=" ^python@3.10.2"
 export mySpackLogs="${SPACK_ROOT}/${USER}/build-logs"
 
 echo "\${myCompiler}  = ${myCompiler}"
