95205349e968e89701c95511578d5fb652a700e2
Daniel M. Topa <dantopa@gmail.com>
2022-03-25 17:54:59 -0600

    yet another centos build

---
+++ SpWx/scripts-shell/spwx-build-xiuhcoatl.sh
@@ -0,0 +1,162 @@
+#! /usr/bin/env bash
+printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
+
+# Wed Feb 16 10:26:35 MST 2022
+
+# source         /repos/github/builds/SpWx/scripts-shell/spwx-build-xiuhcoatl.sh 2>&1 | tee -a ${scratch}/spwx-build-xiuhcoatl.txt
+# source /Volumes/repos/github/builds/SpWx/scripts-shell/spwx-build-xiuhcoatl.sh 2>&1 | tee -a ${scratch}/spwx-build-xiuhcoatl.txt
+source ${repo_build}/scripts-spack/shared/common-header.sh
+
+export buildSeconds=${SECONDS}
+
+# https://swe-gitlab.aer-govcloud.net/afrl-support/SpWx
+#   Readme.md
+#   Maintainer
+
+new_step "Update the SpWx source directory in ${scratch}"
+sub_step_counter=0
+
+#     sub_step 'rm -rf "${scratch}/SpWx"'
+#               rm -rf "${scratch}/SpWx"
+
+    # sub_step "cp -a /SpWx ${scratch}"
+    #           cp -a /SpWx ${scratch}
+
+    # sub_step "cp -a /Volumes/repos/gitlab/SpWx ${scratch}"
+    #           cp -a /Volumes/repos/gitlab/SpWx ${scratch}
+# 
+#     sub_step "cp -a /repos/gitlab/SpWx ${scratch}"
+#               cp -a /repos/gitlab/SpWx ${scratch}
+
+    sub_step 'export localSpWx="${scratch}/SpWx"'
+              export localSpWx="${scratch}/SpWx"
+
+    sub_step "Verify SpWx directory"
+              echo "\${localSpWx}=${localSpWx}"
+
+    sub_step 'cd "${localSpWx}/source"'
+              cd "${localSpWx}/source"
+
+    sub_step "pwd = $PWD"
+
+new_step "Check cmake version: cmake_minimum_required = CMake 3.14"
+sub_step_counter=0
+
+    sub_step "cmake3 --version"
+              cmake3 --version
+
+pause
+
+    sub_step "git pull"
+              git pull
+
+    sub_step "export timerFile=${localSpWx}/build-time.txt"
+              export timerFile=${localSpWx}/build-time.txt
+
+    sub_step "git checkout magfield_update"
+              git checkout magfield_update
+
+new_step "Verify configuration"
+sub_step_counter=0
+
+    sub_step "git branch -a"
+              git branch -a
+
+    sub_step "git status"
+              git status
+
+    sub_step "grab hash for commit: git rev-parse --verify HEAD"
+                                    git rev-parse --verify HEAD
+
+    sub_step "grab hash for commit: git log --pretty=format:'%h' -n 1"
+                                    git log --pretty=format:'%h' -n 1
+
+    sub_step "gcc --version"
+              gcc --version
+
+    sub_step "lsb_release -a"
+              lsb_release -a
+
+new_step "Build SpWx"
+sub_step_counter=0
+
+    sub_step "cd ${localSpWx}"
+              cd ${localSpWx}
+
+    sub_step 'rm -rf "${localSpWx}/build"'
+              rm -rf "${localSpWx}/build"
+
+    sub_step "mkdir build; cd build"
+              mkdir build; cd build
+
+    sub_step "export timerFile=${localSpWx}/build/build-time.txt"
+              export timerFile=${localSpWx}/build/build-time.txt
+
+    sub_step "cmake3 ../source -DDEFINE_DEBUG=ON -DCMAKE_INSTALL_PREFIX=../"
+              cmake3 ../source -DDEFINE_DEBUG=ON -DCMAKE_INSTALL_PREFIX=../
+
+new_step "make; make test; make install"
+sub_step_counter=0
+
+    sub_step "make"
+              make
+
+    sub_step "make test"
+              make test
+
+    sub_step "make install"
+              make install
+
+new_step "Run benchmarks"
+sub_step_counter=0
+
+    sub_step "cd ${localSpWx}"
+              cd ${localSpWx}
+
+new_step 'remove previous benchmarks: find . -name "cpp.benchmark*" | xargs rm'
+                                      find . -name "cpp.benchmark*" | xargs rm
+
+export benchmarkSeconds=${SECONDS}
+
+new_step "Fortran benchmarks"
+sub_step_counter=0
+    sub_step "create cmag.benchmark.fortran: ./bin/satMagCover"
+                                             ./bin/satMagCover
+
+    sub_step "verify file was created: ls -alh cmag.benchmark.fortran"
+                                       ls -alh cmag.benchmark.fortran
+
+    sub_step "create cmag.benchmark.fortran.transform: ./bin/satMagCover transform"
+                                                       ./bin/satMagCover transform
+
+    sub_step "verify file was created: ls -alh cmag.benchmark.transform.fortran"
+                                       ls -alh cmag.benchmark.transform.fortran
+
+new_step "C++ benchmarks"
+sub_step_counter=0
+    sub_step "create cmag.benchmark.cpp: ./bin/satMagCover usecpp"
+                                         ./bin/satMagCover usecpp
+
+    sub_step "verify file was created: ls -alh cmag.benchmark.cpp"
+                                       ls -alh cmag.benchmark.cpp
+
+    sub_step "create cmag.benchmark.cpp.transform: ./bin/satMagCover transform usecpp"
+                                                   ./bin/satMagCover transform usecpp
+
+    sub_step "verify file was created: ls -alh cmag.benchmark.transform.cpp"
+                                       ls -alh cmag.benchmark.transform.cpp
+
+new_step 'SHA256: find . -name "cpp.benchmark*" | xargs shasum -a 256'
+                  find . -name "cpp.benchmark*" | xargs shasum -a 256
+
+new_step "mv ${scratch}/spwx-build-xiuhcoatl.txt ${scratch}/$(uname -n)-${ymdtf}-spwx-build-xiuhcoatl.txt"
+          mv ${scratch}/spwx-build-xiuhcoatl.txt ${scratch}/$(uname -n)-${ymdtf}-spwx-build-xiuhcoatl.txt
+
+new_step "Output stream is in ${scratch}/$(uname -n)-${ymdtf}-spwx-build-xiuhcoatl.txt"
+
+export benchmarkSeconds=$((${SECONDS}-${benchmarkSeconds}))
+printf 'time to run benchmarks: %dh:%dm:%ds\n' $((${benchmarkSeconds}/3600)) $((${benchmarkSeconds}%3600/60)) $((${benchmarkSeconds}%60)) | tee -a ${timerFile}
+
+new_step "Run time:"
+export buildSeconds=$((${SECONDS}-${buildSeconds}))
+printf 'time to build SpWx and run benchmarks: %dh:%dm:%ds\n' $((${buildSeconds}/3600)) $((${buildSeconds}%3600/60)) $((${buildSeconds}%60)) | tee -a ${timerFile}
---
+++ scripts-spack/environment/builders/plus-centos-4.8.5.sh
@@ -0,0 +1,104 @@
+#! /usr/bin/env bash
+printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
+
+# source ${repo_scripts_spack}/environment/builders/plus-centos-4.8.5.sh
+
+# start timer
+export master=${SECONDS}
+# export  myPython="^python@3.10.2"
+# export myOpenMPI="^openmpi@4.1.2"
+
+source /repos/github/builds/scripts-docker/bash-inits/paths.sh
+source ${repo_scripts_spack}/shared/common-header.sh
+
+export dlogs="${dlogs}"
+
+export build_target="llvm@13.0.1"
+
+new_step "Build ${build_target}"
+export sub_step_counter=0
+fileName="${build_target}.txt"
+    sub_step "spack spec ${build_target}"
+              spack spec ${build_target} > ${SPACK_ROOT}/${USER}/specs/${fileName} &
+
+    sub_step "spack info llvm"
+              spack info llvm > ${SPACK_ROOT}/${USER}/info/${fileName}.txt &
+
+    sub_step "spack install ${build_target}"
+              date                      > ${dlogs}/${fileName}
+              echo "${BASH_SOURCE[0]}" >> ${dlogs}/${fileName}
+              echo "spack install ${build_target} ${myCompiler} ${myPython} 2>&1 | tee -a ${dlogs}/${fileName}"
+                    spack install ${build_target} ${myCompiler} ${myPython} 2>&1 | tee -a ${dlogs}/${fileName}
+
+    sub_step "spack compiler find $(spack location -i ${build_target})"
+              spack compiler find $(spack location -i ${build_target})
+
+export build_target="py-tqdm"
+new_step "Build python application ${build_target}"
+export sub_step_counter=0
+fileName="${build_target}.txt"
+    sub_step "spack spec ${build_target}"
+              spack spec ${build_target} > ${SPACK_ROOT}/${USER}/specs/${fileName} &
+
+    sub_step "spack info ${build_target}"
+              spack info ${build_target} > ${SPACK_ROOT}/${USER}/info/${fileName} &
+
+    sub_step "spack install ${build_target} ${myOpenMPI}"
+              date                      > ${dlogs}/${fileName}
+              echo "${BASH_SOURCE[0]}" >> ${dlogs}/${fileName}
+              echo "spack install ${build_target} ${myCompiler} ${myPython} 2>&1 | tee -a ${dlogs}/${fileName}"
+                    spack install ${build_target} ${myCompiler} ${myPython} 2>&1 | tee -a ${dlogs}/${fileName}
+
+export build_target="py-urllib3"
+new_step "Build python application ${build_target}"
+export sub_step_counter=0
+fileName="${build_target}.txt"
+    sub_step "spack spec ${build_target}"
+              spack spec ${build_target} > ${SPACK_ROOT}/${USER}/specs/${fileName} &
+
+    sub_step "spack info ${build_target}"
+              spack info ${build_target} > ${SPACK_ROOT}/${USER}/info/${fileName} &
+
+    sub_step "spack install ${build_target} ${myCompiler} ${myPython}"
+              date                      > ${dlogs}/${fileName}
+              echo "${BASH_SOURCE[0]}" >> ${dlogs}/${fileName}
+
+              echo "spack install ${build_target} ${myCompiler} ${myPython} 2>&1 | tee -a ${dlogs}/${fileName}"
+                    spack install ${build_target} ${myCompiler} ${myPython} 2>&1 | tee -a ${dlogs}/${fileName}
+
+export build_target="py-virtualenv"
+new_step "Build python application ${build_target}"
+export sub_step_counter=0
+fileName="${build_target}.txt"
+    sub_step "spack spec ${build_target}"
+              spack spec ${build_target} > ${SPACK_ROOT}/${USER}/specs/${fileName} &
+
+    sub_step "spack info ${build_target}"
+              spack info ${build_target} > ${SPACK_ROOT}/${USER}/info/${fileName} &
+
+    sub_step "spack install ${build_target} ${myCompiler} ${myPython} ${myOpenMPI}"
+              date                      > ${dlogs}/${fileName}
+              echo "${BASH_SOURCE[0]}" >> ${dlogs}/${fileName}
+              echo "spack install ${build_target} ${myCompiler} ${myPython} ${myOpenMPI} 2>&1 | tee -a ${dlogs}/${fileName}"
+                    spack install ${build_target} ${myCompiler} ${myPython} ${myOpenMPI} 2>&1 | tee -a ${dlogs}/${fileName}
+
+export build_target="py-astropy"
+new_step "Build python application ${build_target}"
+export sub_step_counter=0
+fileName="${build_target}.txt"
+    sub_step "spack spec ${build_target}+extras ${myPython}"
+              spack spec ${build_target}+extras ${myPython} > ${SPACK_ROOT}/${USER}/specs/${fileName} &
+
+    sub_step "spack info ${build_target}"
+              spack info ${build_target} > ${SPACK_ROOT}/${USER}/info/${fileName} &
+
+    sub_step "spack install ${build_target}+extras ${myCompiler} ${myPython}"
+              date                      > ${dlogs}/${fileName}
+              echo "${BASH_SOURCE[0]}" >> ${dlogs}/${fileName}
+              echo "spack install ${build_target}+extras ${myCompiler} ${myPython} 2>&1 | tee -a ${dlogs}/${fileName}"
+                    spack install ${build_target}+extras ${myCompiler} ${myPython} 2>&1 | tee -a ${dlogs}/${fileName}
+
+wait
+
+export master=$((${SECONDS}-${master}))
+printf 'time for all builds: %dh:%dm:%ds\n' $((${master}/3600)) $((${master}%3600/60)) $((${master}%60))
