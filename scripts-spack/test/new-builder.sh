c969debe14f7bdfab7ef185b2a4bb9d6233a502b
Daniel M. Topa <dantopa@gmail.com>
2022-04-08 13:37:20 -0600

    Update new-builder.sh

--- scripts-spack/test/new-builder.sh
+++ scripts-spack/test/new-builder.sh
@@ -5,6 +5,8 @@ printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
 export master=${SECONDS}
 
+export mySpackLogs="/Volumes/spacktivity/xiuhcoatl-monterey-spack/dantopa"
+
 source /repos/github/builds/scripts-docker/bash-inits/paths.sh
 source ${repo_scripts_spack}/shared/common-header.sh
 
@@ -12,7 +14,7 @@ spack_test(){
 # https://stackoverflow.com/questions/4423306/how-do-i-find-the-number-of-arguments-passed-to-a-bash-script
     sub_step_counter=$((sub_step_counter+1))
     numArgs="$#"
-    echo "numArgs = ${numArgs}"
+    # echo "numArgs = ${numArgs}"
     # build command line
     cmdLine=""
     for var in "$@"
@@ -25,11 +27,13 @@ spack_test(){
     clean=$(echo "${cmdLine}" | tr -d ' ')
     clean=$(echo "${clean}"   | sed 's/\^/-/g')
     clean=$(echo "${clean}"   | sed 's/\%/-/g')
-    echo "\${cmdLine} = ${cmdLine}"
-    echo "\${clean} = ${clean}"
-    echo "spack info ${1} > ${mySpackLogs}/info/${1}.txt"
-    echo "spack spec ${cmdLine}/spec/${1}.txt"
-    echo "  ${step_counter}.${sub_step_counter}: spack install ${cmdLine}"
+    # echo "\${cmdLine} = ${cmdLine}"
+    # echo "\${clean} = ${clean}"
+    echo ""
+    echo "  ${step_counter}.${sub_step_counter}: spack install ${1}"
+    echo "spack info ${1} >> ${mySpackLogs}/info/${1}.txt"
+    echo "spack spec ${cmdLine} >> ${mySpackLogs}/spec/${clean}.txt"
+    echo "spack install ${cmdLine}"
 }
 
 sub_step_counter=1
@@ -38,7 +42,11 @@ spack_test "octave" " +arpack +fftw" "${myCompiler}" "${myOpenMPI}"
 
 spack_test "gcc" "@11.2.0" "${myCompiler}" "${myOpenMPI}"
 
+new_step "Basic installs: ${myCompiler}"
+spack_sub_step_counter=0
 
+
+wait
 
 new_step "print wall time used"
   export master=$((${SECONDS}-${master}))
