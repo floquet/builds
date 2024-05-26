c1d7ffa387087dae26163f498ebcb4caba0907bd
Daniel Topa <dantopa@gmail.com>
2024-05-25 21:20:21 -0600

    more spack tweaks

--- scripts-spack/environment/slave-spack-build.sh
+++ scripts-spack/environment/slave-spack-build.sh
@@ -17,24 +17,29 @@ spack info fftw                > ${dirInfo}/fftw.txt  2>&1 &
 spack info gdb                 > ${dirInfo}/gdb.txt  2>&1 &
 spack info gh                  > ${dirInfo}/go.txt  2>&1 &
 spack info go                  > ${dirInfo}/go.txt  2>&1 &
+spack info gsl                 > ${dirInfo}/gsl.txt  2>&1 &
 spack info julia               > ${dirInfo}/julia.txt  2>&1 &
 spack info h5cpp               > ${dirInfo}/h5cpp.txt  2>&1 &
 spack info h5part              > ${dirInfo}/h5part.txt  2>&1 &
 spack info h5z-zfp             > ${dirInfo}/h5z-zfp.txt  2>&1 &
+spack info librsb              > ${dirInfo}/librsb.txt  2>&1 &
 spack info lua                 > ${dirInfo}/lua.txt  2>&1 &
 spack info meson               > ${dirInfo}/meson.txt  2>&1 &
 spack info netcdf-c            > ${dirInfo}/netcdf-c.txt  2>&1 &
 spack info netcdf-fortran      > ${dirInfo}/netcdf-fortran.txt  2>&1 &
 spack info netcdf-cxx4         > ${dirInfo}/netcdf-cxx4.txt  2>&1 &
 spack info netlib-scalapack    > ${dirInfo}/netlib-scalapack.txt  2>&1 &
+spack info ntl                 > ${dirInfo}/ntl.txt  2>&1 &
 spack info opencoarrays        > ${dirInfo}/opencoarrays.txt  2>&1 &
 spack info parallel-netcdf     > ${dirInfo}/parallel-netcdf.txt  2>&1 &
 spack info r                   > ${dirInfo}/r.txt  2>&1 &
 spack info rust                > ${dirInfo}/rust.txt  2>&1 &
 spack info ruby                > ${dirInfo}/ruby.txt  2>&1 &
 spack info scala               > ${dirInfo}/scala.txt  2>&1 &
+spack info sprng               > ${dirInfo}/sprng.txt  2>&1 &
 spack info tau                 > ${dirInfo}/tau.txt  2>&1 &
 spack info valgrind            > ${dirInfo}/valgrind.txt  2>&1 &
+spack info xtensor             > ${dirInfo}/xtensor.txt  2>&1 &
 spack info zoltan              > ${dirInfo}/zoltan.txt  2>&1 &
 
 # # #   INSTALL
@@ -49,6 +54,7 @@ spack install fftw ${myCompiler}                 | tee ${dirInstall}/fftw.txt 2>
 spack install gdb ${myCompiler} ${myPython}      | tee ${dirInstall}/gdb.txt 2>&1
 spack install gh  ${myCompiler}                  | tee ${dirInstall}/gh.txt 2>&1
 spack install go  ${myCompiler}                  | tee ${dirInstall}/go.txt 2>&1
+spack install gsl ${myCompiler}                  | tee ${dirInstall}/gsl.txt 2>&1
 spack install julia ${myCompiler} ${myLLVM}      | tee ${dirInstall}/julia.txt 2>&1
 
 spack install h5bench ${myCompiler} ${myOpenMPI}          | tee ${dirInstall}/h5bench.txt  2>&1
@@ -57,6 +63,7 @@ spack install h5hut ${myCompiler} ${myOpenMPI}            | tee ${dirInstall}/h5
 spack install h5part ${myCompiler} ${myOpenMPI}           | tee ${dirInstall}/h5part.txt  2>&1
 spack install h5z-zfp ${myCompiler}                       | tee ${dirInstall}/h5z-zfp.txt  2>&1
 
+spack install librsb ${myCompiler}                        | tee ${dirInstall}/librsb.txt  2>&1
 spack install lua ${myCompiler}                           | tee ${dirInstall}/lua.txt  2>&1
 spack install meson ${myCompiler} ${myPython}             | tee ${dirInstall}/meson.txt  2>&1
 
@@ -64,6 +71,7 @@ spack install netcdf-c ${myCompiler} ${myOpenMPI}         | tee ${dirInstall}/ne
 spack install netcdf-fortran ${myCompiler}                | tee ${dirInstall}/netcdf-fortran.txt 2>&1
 spack install netcdf-cxx4 ${myCompiler} ${myPython}       | tee ${dirInstall}/netcdf-cxx4.txt 2>&1
 spack install netlib-scalapack ${myCompiler} ${myOpenMPI} | tee ${dirInstall}/netlib-scalapack.txt  2>&1
+spack install ntl ${myCompiler}                           | tee ${dirInstall}/ntl.txt 2>&1
 
 spack install opencoarrays ${myCompiler} ${myOpenMPI}     | tee ${dirInstall}/opencoarrays.txt 2>&1
 spack install parallel-netcdf ${myCompiler} ${myOpenMPI}  | tee ${dirInstall}/parallel-netcdf.txt 2>&1
@@ -72,8 +80,10 @@ spack install r ${myCompiler}                             | tee ${dirInstall}/r.
 spack install ruby ${myCompiler}                          | tee ${dirInstall}/ruby.txt 2>&1
 spack install rust ${myCompiler} ${myPython}              | tee ${dirInstall}/rust.txt 2>&1
 spack install scala ${myCompiler}                         | tee ${dirInstall}/scala.txt 2>&1
+spack install sprng ${myCompiler}                         | tee ${dirInstall}/sprng.txt 2>&1
 spack install tau  ${myCompiler} ${myPython} ${myOpenMPI} | tee ${dirInstall}/tau.txt 2>&1
 spack install valgrind ${myCompiler} ${myOpenMPI}         | tee ${dirInstall}/valgrind.txt 2>&1
+spack install xtensor ${myCompiler}                       | tee ${dirInstall}/xtensor.txt 2>&1
 spack install zoltan ${myCompiler} ${myOpenMPI}           | tee ${dirInstall}/zoltan.txt 2>&1
 
 spack install llvm@16.0.6 ${myCompiler} ${myPython} | tee ${dirInstall}/llvm@16.0.6.txt 2>&1
