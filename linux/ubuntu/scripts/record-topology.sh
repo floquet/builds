5aa9a22dc64734e80df215aeace22aff64354fd7
Daniel Topa <dantopa@gmail.com>
2026-08-19 23:41:26 -0600

    Update Ubuntu configuration: isomer

--- linux/ubuntu/scripts/record-topology.sh
+++ linux/ubuntu/scripts/record-topology.sh
@@ -40,7 +40,8 @@ run_topology() {
         --force \
         --no-io \
         "$@" \
-        2> "${message_file}"
+        # 2> "${message_file}"
+        > "${message_file}" 2>&1
     then
         printf "%s  %-20s OK       time=%d seconds\n" \
             "$(date)" "${name}" "$((SECONDS - start))" >> "${pLog}"
