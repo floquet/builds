ae36c71759f372bbdd790c6cc5d1f5adb6c05ed5
Daniel Topa <dantopa@gmail.com>
2026-08-20 00:13:43 -0600

    Update Ubuntu configuration: isomer

--- linux/ubuntu/scripts/recorder.sh
+++ linux/ubuntu/scripts/recorder.sh
@@ -2,15 +2,16 @@
 printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
 : "${pSelf:?pSelf is not defined}"
+
 # locate recorder.sh to locate the record-*.sh files
 pRecorder="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-record-system.sh      OS, kernel, CPU summary, RAM summary,
-                      disks, filesystems, mounts
-record-proc.sh        raw kernel /proc information
-record-topology.sh    CPU/cache/NUMA hardware topology
-record-network.sh     interfaces, routes, DNS, sockets
-record-toolchain.sh   compilers and development tools
+# record-system.sh      OS, kernel, CPU summary, RAM summary,
+#                       disks, filesystems, mounts@
+# record-proc.sh        raw kernel /proc information
+# record-topology.sh    CPU/cache/NUMA hardware topology
+# record-network.sh     interfaces, routes, DNS, sockets
+# record-toolchain.sh   compilers and development tools
 
 # Shell-state recorders must be sourced so they can see
 # aliases, functions, shell variables, and shell options.
