fa12d26447342dde335462acf0bd6e73238e7784
Daniel Topa <dantopa@gmail.com>
2026-08-21 00:30:41 -0600

    Update Ubuntu configuration: isomer

--- linux/ubuntu/scripts/arXiver.sh
+++ linux/ubuntu/scripts/arXiver.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
+printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
-# set -euo pipefail
-
-printf '%s, %s\n' \
-    "$(date '+%Y-%m-%d %H:%M:%S')" \
-    "$(basename "${BASH_SOURCE[0]}")"
-
+# Require pInfo to be set and non-empty; otherwise print the error and terminate.
 : "${pInfo:?pInfo is not set}"
 
 dest="$pInfo/arXive"
 mkdir -p "$dest"
 
-cp -- "$HOME/${HOSTNAME}.sh" "$dest/"
-cp -- "$HOME/.vimrc"         "$dest/"
-cp -- "$HOME/.gitconfig.sh"  "$dest/"
+cp -- "$HOME/.${HOSTNAME}.sh" "$dest/"
+cp -- "$HOME/.vimrc"          "$dest/"
+cp -- "$HOME/.gitconfig"      "$dest/"
