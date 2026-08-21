3ab40d6f890889daf85bf3e8e8234434fce365c8
Daniel Topa <dantopa@gmail.com>
2026-08-20 23:23:15 -0600

    Update Ubuntu configuration: isomer

--- linux/ubuntu/scripts/alias.sh
+++ linux/ubuntu/scripts/alias.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
-alias lss='ls -alh'
+alias lss='ls -alh'i
+alias wolf="/usr/local/Wolfram/Wolfram/15.0/Executables/Wolfram"
+alias nb="/usr/local/Wolfram/Wolfram/15.0/Executables/WolframNB"
 
 # source/reload
 alias sr='echo "source ${HOME}/.${HOSTNAME}.sh"; source "${HOME}/.${HOSTNAME}.sh"'
--- linux/ubuntu/scripts/arXiver.sh
+++ linux/ubuntu/scripts/arXiver.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 printf "%s\n" "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
-mkdir -p $pInfo/:x
+mkdir -p $pInfo/arXive
 
-
+cp ~/.${HOSTNAME}.sh ${pInfo}/arXive/.
+cp ~/.vimrc          ${pInfo}/arXive/.
+cp ~/.gitconfig.sh   ${pInfo}/arXive/.
 
 
