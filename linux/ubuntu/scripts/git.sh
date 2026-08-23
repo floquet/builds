9b4048019fd7b62aad229e2326d03768da0d3ef5
Daniel Topa <dantopa@gmail.com>
2026-08-23 15:02:43 -0600

    Update Ubuntu configuration: isomer

--- linux/ubuntu/scripts/git.sh
+++ linux/ubuntu/scripts/git.sh
@@ -16,4 +16,29 @@ function commit-builds() {
     popd > /dev/null
 }
 
+function config_git(){
+    echo "Setting Git configurations for Daniel Topa..."
+
+    # Identity
+    git config --global user.name "Daniel Topa"
+    git config --global user.email "dantopa@gmail.com"
+
+    # Branch behavior
+    git config --global init.defaultBranch main
+    git config --global pull.rebase false
+    git config --global push.default simple
+    git config --global push.autoSetupRemote true
+
+    # Interface
+    git config --global color.ui auto
+    git config --global core.editor vim
+    git config --global core.pager "less -FRX"
+
+    # Merge behavior
+    git config --global merge.tool meld
+    git config --global rerere.enabled true
+
+    echo "Git configuration for Daniel Topa is complete."
+}
+
 
